45ac5c122c02c4d0d2d02c8ec28279280878da11
Jaime Niswonger <jniswonger@keyholesoftware.com>
2022-08-11 18:43:42 +0000

    implemented credits with feature  flag

--- src/feature-flags.ts
+++ src/feature-flags.ts
@@ -4,8 +4,17 @@ interface FeatureFlags {
   };
 }
 
+const checkFeatureFlag = (featureFlag: string | undefined): boolean => {
+  console.log(process.env.ALL_FEATURES_ON);
+  const featureFlagOn = process.env.ALL_FEATURES_ON === 'true'
+    ? true
+    : featureFlag === 'true' || false
+  console.log(`Feature Flag: %s`, featureFlagOn);
+  return featureFlagOn;
+}
+
 export const featureFlags: FeatureFlags = {
   credits: {
-    enabled: process.env.CREDITS_ENABLED === 'true' || false,
+    enabled: checkFeatureFlag(process.env.CREDITS_ENABLED)
   },
 };
