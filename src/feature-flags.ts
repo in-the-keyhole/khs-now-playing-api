cac9c995b7ffe347897ab39a17e42833093525a7
Jaime Niswonger <jniswonger@keyholesoftware.com>
2022-08-16 03:59:06 +0000

    added credit feature

--- src/feature-flags.ts
+++ src/feature-flags.ts
@@ -5,11 +5,9 @@ interface FeatureFlags {
 }
 
 const checkFeatureFlag = (featureFlag: string | undefined): boolean => {
-  console.log(process.env.ALL_FEATURES_ON);
   const featureFlagOn = process.env.ALL_FEATURES_ON === 'true'
     ? true
     : featureFlag === 'true' || false
-  console.log(`Feature Flag: %s`, featureFlagOn);
   return featureFlagOn;
 }
 
